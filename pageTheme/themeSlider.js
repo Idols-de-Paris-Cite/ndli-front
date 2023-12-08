fe673cff151b8f9acd6d2720042eb6d1de6d5a67
Noe <noegodin443@gmail.com>
2023-12-08 02:10:09 +0100

    Meilleur fonctionnement dégradé

--- pageTheme/themeSlider.js
+++ pageTheme/themeSlider.js
@@ -1,11 +1,11 @@
 document.getElementById("red-slider").addEventListener("input", updateColor);
 document.getElementById("green-slider").addEventListener("input", updateColor);
 document.getElementById("blue-slider").addEventListener("input", updateColor);
-document.getElementById("toggleGradientButton").addEventListener("click", toggleGradient);
+document.getElementById("toggleGradientCheckbox").addEventListener("change", toggleGradient);
 
 let isGradientEnabled = false;
-let gradientColorStart = ""; // Anciennes valeurs des sliders pour le gradient
-let gradientColorEnd = "";   // Nouvelles valeurs des sliders pour le gradient
+let gradientColorStart = "rgb(0, 0, 0)"; // Couleur initiale du gradient
+let gradientColorEnd = `rgb(${document.getElementById("red-slider").value}, ${document.getElementById("green-slider").value}, ${document.getElementById("blue-slider").value})`;
 
 function updateColor() {
     let red = document.getElementById("red-slider").value;
@@ -16,11 +16,12 @@ function updateColor() {
     document.getElementById("current-valueG").innerText = green;
     document.getElementById("current-valueB").innerText = blue;
 
+    gradientColorEnd = `rgb(${red}, ${green}, ${blue})`;
+
     if (isGradientEnabled) {
-        // Si le gradient est activé, utilisez le gradient avec les anciennes et nouvelles valeurs des sliders
         document.body.style.background = `linear-gradient(to right, ${gradientColorStart}, ${gradientColorEnd})`;
     } else {
-        // Si le gradient est désactivé, utilisez simplement la couleur du fond
+        gradientColorStart = `rgb(${red}, ${green}, ${blue})`;
         document.body.style.background = `rgb(${red}, ${green}, ${blue})`;
     }
 }
@@ -29,13 +30,8 @@ function toggleGradient() {
     isGradientEnabled = !isGradientEnabled;
 
     if (isGradientEnabled) {
-        // Activer le gradient (stocker les anciennes valeurs des sliders)
-        document.getElementById("toggleGradientButton").innerText = "Désactiver le Gradient";
-        gradientColorStart = document.body.style.background; // Stocker la couleur actuelle du fond
+        gradientColorStart = document.body.style.background-color;
     } else {
-        // Désactiver le gradient (mettre à jour les nouvelles valeurs des sliders)
-        document.getElementById("toggleGradientButton").innerText = "Activer le Gradient";
-        gradientColorEnd = `rgb(${document.getElementById("red-slider").value}, ${document.getElementById("green-slider").value}, ${document.getElementById("blue-slider").value})`;
         updateColor(); // Mettre à jour la couleur si le gradient est désactivé
     }
 }
