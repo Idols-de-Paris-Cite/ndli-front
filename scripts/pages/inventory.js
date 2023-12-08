ca0a5f02e9cc3e7ece233bce26f03276fd02ce79
Tony <tony.guan@etu.u-paris.fr>
2023-12-08 04:36:30 +0100

    responsive

--- scripts/pages/inventory.js
+++ scripts/pages/inventory.js
@@ -2,30 +2,31 @@ var inventoryShown = false;
 
 window.addEventListener('load', function(e) {
     let button = document.getElementById("btn_inv");
-    // let chapeau = document.getElementById("chapeau_trouve");
-    // let tete = document.getElementById("tete_trouve");
-    // let corps = document.getElementById("corps_trouve");
-    // let jambes = document.getElementById("jambes_trouve");
 
-    // chapeau.addEventListener("click", function(e) {
-    //     let chapeauInventory = document.getElementById("chapeau");
-    //     chapeauInventory.style.filter = "brightness(100%)";
-    // });
+     let chapeau = document.getElementById("chapeau_trouve");
+     let tete = document.getElementById("tete_trouve");
+     let corps = document.getElementById("corps_trouve");
+     let jambes = document.getElementById("jambes_trouve");
 
-    // tete.addEventListener("click", function(e) {
-    //     let teteInventory = document.getElementById("tete");
-    //     teteInventory.style.filter = "brightness(100%)";
-    // });
+     chapeau.addEventListener("click", function(e) {
+         let chapeauInventory = document.getElementById("chapeau");
+         chapeauInventory.style.filter = "brightness(100%)";
+     });
 
-    // corps.addEventListener("click", function(e) {
-    //     let corpsInventory = document.getElementById("corps");
-    //     corpsInventory.style.filter = "brightness(100%)";
-    // });
+     tete.addEventListener("click", function(e) {
+         let teteInventory = document.getElementById("tete");
+         teteInventory.style.filter = "brightness(100%)";
+     });
 
-    // jambes.addEventListener("click", function(e) {
-    //     let jambesInventory = document.getElementById("jambes");
-    //     jambesInventory.style.filter = "brightness(100%)";
-    // });
+     corps.addEventListener("click", function(e) {
+         let corpsInventory = document.getElementById("corps");
+         corpsInventory.style.filter = "brightness(100%)";
+     });
+
+     jambes.addEventListener("click", function(e) {
+         let jambesInventory = document.getElementById("jambes");
+        jambesInventory.style.filter = "brightness(100%)";
+     });
 
 
     button.addEventListener("click", function(e) {
